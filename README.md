f6d74e11c64e094648f5f9dd09527c8673d2f0c6
hykw <hykw1234@gmail.com>
2020-11-06 08:26:06 +0000

    DEBUGコード削除

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # hykw-wp-workaround-ssl
+
+mu-plugins/main.php として置いちゃえばいいんちゃうかな
--- main.php
+++ main.php
@@ -4,18 +4,9 @@
   Plugin URI: https://github.com/hykw/hykw-wp-workaround-ssl
   Description: wp-config.php をいじれない環境で $_SERVER['HTTPS']='on'; しちゃうプラグイン
   Author: hitoshi-hayakawa
-  Version: 1.0.5
+  Version: 1.0.6
 */
 
-echo 'xxxxxxxx: debug1';
-$_SERVER['HTTPS']='on';
-
-add_action( 'muplugins_loaded', function () {
-  echo 'xxxxxxxx: debug2';
-  $_SERVER['HTTPS']='on';
-});
-
 add_action( 'plugin_loaded', function () {
-  echo 'xxxxxxxx: debug3';
   $_SERVER['HTTPS']='on';
 });
