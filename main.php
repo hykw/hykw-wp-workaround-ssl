3c9da8e486ab91c4448d32307f0a6989fff82315
hykw <hykw1234@gmail.com>
2020-11-06 08:04:33 +0000

    muplugins_loadedに移動

--- main.php
+++ main.php
@@ -4,12 +4,12 @@
   Plugin URI: https://github.com/hykw/hykw-wp-workaround-ssl
   Description: wp-config.php をいじれない環境で $_SERVER['HTTPS']='on'; しちゃうプラグイン
   Author: hitoshi-hayakawa
-  Version: 1.0.2
+  Version: 1.0.3
 */
 
 echo 'xxxxxxxx: debug1';
 
-add_action( 'mu_plugin_loaded', function () {
+add_action( 'muplugins_loaded', function () {
   echo 'xxxxxxxx: debug2';
   $_SERVER['HTTPS']='on';
 });
